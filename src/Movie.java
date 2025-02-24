f01b2b0aa1d560adc3dc6189a724b958a2a9d1e1
BT_1N3_15 <manihaa@nycstudents.net>
2025-02-24 10:10:42 -0500

    movie data model class complete

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,41 @@
 public class Movie {
+    private String title;
+    private String cast;
+    private String director;
+    private String overview;
+    private int runTime;
+    private double userRating;
+
+    public Movie(String t, String c, String d, String o, int r, double u){
+        title = t;
+        cast = c;
+        director = d;
+        overview = o;
+        runTime = r;
+        userRating = u;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getCast(){
+        return cast;
+    }
+
+    public String getDirector(){
+        return director;
+    }
+
+    public String getOverview(){
+        return overview;
+    }
+
+    public int getRunTime(){
+        return runTime;
+    }
+
+    public double getUserRating(){
+        return userRating;
+    }
 }
