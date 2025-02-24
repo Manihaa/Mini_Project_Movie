d881737953a18cbc619650ec4d93979332586635
BT_1N3_15 <manihaa@nycstudents.net>
2025-02-24 10:43:49 -0500

    Task 3b complete main menu and a bunch of other methods

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,23 +1,62 @@
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MovieCollection {
 
     private ArrayList<Movie> collection;
-    private Scanner scan;
+    private Scanner scanner;
 
     public MovieCollection(){
         collection = new ArrayList<Movie>();
-        scan = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         start();
     }
 
     private void start(){
         readData();
-        System.out.println(collection);
+
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scanner.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+
+    }
+
+    private String searchTitles(){
+
+    }
+
+    private String searchCast(){
+
+    }
+
+    private String movieInfo(Movie m){
+        String str = "Title: " + m.getTitle() +
+                "\nCast: " + m.getCast() +
+                "\nDirector: " + m.getDirector() +
+                "\nOverview: " + m.getOverview() +
+                "\nRuntime: " + m.getRunTime() +
+                "\nUser Rating: " + m.getUserRating();
+        return str;
     }
 
     private void readData(){
